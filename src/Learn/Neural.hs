cc28b2bd08f0bbd157e6d37d9e9fcc95e7d9bf88
Justin Le <justin@jle.im>
2017-03-16 23:08:08 -0700

    omg everything is a mess

--- src/Learn/Neural.hs
+++ src/Learn/Neural.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs                  #-}
 {-# LANGUAGE LambdaCase             #-}
@@ -10,6 +12,7 @@
 {-# LANGUAGE TypeFamilies           #-}
 {-# LANGUAGE TypeInType             #-}
 {-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE TypeSynonymInstances   #-}
 
 module Learn.Neural (
   ) where
@@ -20,6 +23,7 @@ import           Control.Monad.Primitive
 import           Control.Monad.ST
 import           Data.Bifunctor
 import           Data.Kind
+import           Data.Type.Index
 import           Data.Type.Option
 import           GHC.TypeLits
 import           Numeric.BLAS
@@ -34,13 +38,13 @@ type family MaybeToList (a :: Maybe k) = (b :: [k]) where
     MaybeToList 'Nothing   = '[]
 
 -- type family StateInp (a :: Maybe (BShape Nat -> Type)) (s :: BShape Nat) = (b :: [Type]) where
---     StateInp ('Just t) s = StateInpjkk
+--     StateInp ('Just t) s = StateInp
 
 class Component c b i o | c -> i, c -> o, c -> b where
     type CState  c b i o :: Maybe Type
     runComponent
-        :: BPOp t (b i ': c b i o ': MaybeToList (CState c b i o))
-                  (Tuple (b o ': MaybeToList (CState c b i o)))
+        :: OpB t (b i ': c b i o ': MaybeToList (CState c b i o))
+                 (Tuple (b o ': MaybeToList (CState c b i o)))
     initComponent
         :: ContGen d
         => Sing i
@@ -49,77 +53,105 @@ class Component c b i o | c -> i, c -> o, c -> b where
         -> Gen (PrimState m)
         -> m (c b i o, Option I (CState c b i o))
 
-data NetState = SomeState
-              | NoState
+data NetStateful = Stateful
+                 | NonStateful
 
-data NetComponent :: NetState -> (BShape Nat -> Type) -> BShape Nat -> BShape Nat -> Type where
+data NetComponent :: (BShape Nat -> Type) -> Maybe Type -> BShape Nat -> BShape Nat -> Type where
     NCPure
         :: (Component c b i o, CState c b i o ~ 'Nothing, Num (c b i o))
         => c b i o
-        -> NetComponent s b i o
+        -> NetComponent b 'Nothing i o
     NCStateful
         :: (Component c b i o, CState c b i o ~ 'Just st, Num (c b i o), Num st)
         => c b i o
         -> st
-        -> NetComponent 'SomeState b i o
+        -> NetComponent b ('Just st) i o
 
-runNetComponent
-    :: NetComponent st b i o
-    -> b i
-    -> (b o, NetComponent st b i o)
-runNetComponent = \case
-    n@(NCPure p)   -> \x -> (, n) . getI . head' $ evalBPOp runComponent (x ::< p ::< Ø)
-    NCStateful p s -> \x -> case evalBPOp runComponent (x ::< p ::< s ::< Ø) of
-      I y :< I s' :< Ø -> (y, NCStateful p s')
+-- runNetComponent
+--     :: NetComponent st b i o
+--     -> b i
+--     -> (b o, NetComponent st b i o)
+-- runNetComponent = \case
+--     n@(NCPure p)   -> \x -> (, n) . getI . head' $ evalBPOp runComponent (x ::< p ::< Ø)
+--     NCStateful p s -> \x -> case evalBPOp runComponent (x ::< p ::< s ::< Ø) of
+--       I y :< I s' :< Ø -> (y, NCStateful p s')
+
+-- netComponentOp
+--     :: forall s b st i o. (Num (b i), Num (b o))
+--     => BPOp s '[ b i, NetComponent b st i o ] '[b o, NetComponent b st i o]
+-- netComponentOp = withInps $ \(x :< c :< Ø) ->
+--     withGADT c $ \case
+--       c'@(NCPure p) -> BPC (p ::< Ø) (NCPure . getI . head') $ \(pVar :< Ø) -> do
+--         y :< Ø <- runComponent ~$$ (x :< pVar :< Ø)
+--         return $ y :< constVar c' :< Ø
+--       NCStateful p s -> BPC (p ::< s ::< Ø)
+--                             (\(I gP :< I gS :< Ø) -> NCStateful gP gS)
+--                             $ \(pVar :< sVar :< Ø) -> do
+--         y :< sVar' :< Ø <- runComponent ~$$ (x :< pVar :< sVar :< Ø)
+--         return $ y :< _ :< Ø
+--         -- c' :< Ø <- isoVar (iso (\(I p' :< I s' :< Ø) -> only_ (NCStateful p' s'))
+--         --                        (\case I (NCStateful p' s') :< Ø -> p' ::< s' ::< Ø)
+--         --                   )
+--         -- (c' :: BVar s '[ b i, NetComponent b st i o ] (NetComponent b st i o)) :< Ø
+--         --     <- _
+--             -- <- isoVar (undefined) (pVar :< sVar' :< Ø)
+--             -- <- isoVar (iso (\(I p' :< I s' :< Ø) -> only_ (NCStateful p' s')) _)
+--             --           (pVar :< sVar' :< Ø)
+--         -- return $ y :< _ :< Ø
+--       --   y :< s' :< Ø <- runComponent ~$$ (x :< p' :< Ø)
+
+
+-- instance Every Num as => Num (Tuple as) where
+
+-- netComponentOp = OpM $ \(I x :< I c :< Ø) ->
+--     case c of
+--       n@(NCPure p) -> do
+--         (I y :< Ø, gF) <- runOpM' runComponent (x ::< p ::< Ø)
+--         let out = y ::< n ::< Ø
+--             gF' = fmap (\case I y' :< I p' :< Ø -> y' ::< NCPure p' ::< Ø)
+--                 . gF
+--                 . fmap (\case x' :< _ :< Ø -> x' :< Ø)
+--         return (out, gF')
+--       NCStateful p s -> do
+--         (I y :< I s' :< Ø, gF) <- runOpM' runComponent (x ::< p ::< s ::< Ø)
+--         let out = y ::< NCStateful p s' ::< Ø
+--             gF' = fmap (\case I y' :< I p' :< I s'' :< Ø -> y' ::< NCStateful p' s'' ::< Ø)
+--                 . gF
+--                 . fmap (\case x' :< I (NCStateful _ s'') :< Ø -> x' :< I s'' :< Ø)
+--         return (out, gF')
 
 netComponentOp
-    :: Num (b i)
-    => OpB s '[ b i, NetComponent st b i o ] (Tuple '[b o, NetComponent st b i o])
+    :: forall s b st i o. Num (b i)
+    => OpB s '[ b i, NetComponent b st i o ] (Tuple '[b o, NetComponent b st i o])
 netComponentOp = OpM $ \(I x :< I c :< Ø) ->
     case c of
       n@(NCPure p) -> do
-        (I y :< Ø, gF) <- runOpM' (bpOp runComponent) (x ::< p ::< Ø)
+        (I y :< Ø, gF) <- runOpM' runComponent (x ::< p ::< Ø)
         let out = y ::< n ::< Ø
             gF' = fmap (\case I y' :< I p' :< Ø -> y' ::< NCPure p' ::< Ø)
                 . gF
                 . fmap (\case x' :< _ :< Ø -> x' :< Ø)
         return (out, gF')
       NCStateful p s -> do
-        (I y :< I s' :< Ø, gF) <- runOpM' (bpOp runComponent) (x ::< p ::< s ::< Ø)
+        (I y :< I s' :< Ø, gF) <- runOpM' runComponent (x ::< p ::< s ::< Ø)
         let out = y ::< NCStateful p s' ::< Ø
             gF' = fmap (\case I y' :< I p' :< I s'' :< Ø -> y' ::< NCStateful p' s'' ::< Ø)
                 . gF
-                . fmap (\case x' :< I _ :< Ø -> _)         -- here is the problem
+                . fmap (\case x' :< I (NCStateful _ s'') :< Ø -> x' :< I s'' :< Ø)
         return (out, gF')
 
+-- data Network :: NetState -> (BShape Nat -> Type) -> BShape Nat -> BShape Nat -> Type where
+--     NetExt :: NetComponent st b i o -> Network st b i o
+--     (:&)   :: NetComponent st b i h -> Network st b h o -> Network s b i o
 
-    -- -> b i
-    -- -> ((b o, NetComponent st b i o), b o -> NetComponent st b i o)
--- gradNetComponent = \case
-    -- n@(NCPure p) -> \x -> runST $ do
-    --   (y :< Ø, gF) <- runOpM' (bpOp runComponent) (x ::< p ::< Ø)
-
--- netComponentOp
---     :: NetComponent st b i o
---     -> BPOp s '[ b i, NetComponent st b i o ] (b o)
--- netComponentOp = \case
---     NCPure p -> withInps $ \(x :< p' :< Ø) -> do
---         c :< Ø <- iso (\case NCPure p'' -> only_ p'') _ #<~ p'
---         _
-
-
-data Network :: NetState -> (BShape Nat -> Type) -> BShape Nat -> BShape Nat -> Type where
-    NetExt :: NetComponent st b i o -> Network st b i o
-    (:&)   :: NetComponent st b i h -> Network st b h o -> Network s b i o
-
-runNetwork
-    :: Network st b i o
-    -> b i
-    -> (b o, Network st b i o)
-runNetwork = \case
-    NetExt c -> second NetExt . runNetComponent c
-    c :& n   -> \x -> case runNetComponent c x of
-      (y, c') -> case runNetwork n y of
-        (z, n') -> (z, c' :& n')
+-- runNetwork
+--     :: Network st b i o
+--     -> b i
+--     -> (b o, Network st b i o)
+-- runNetwork = \case
+--     NetExt c -> second NetExt . runNetComponent c
+--     c :& n   -> \x -> case runNetComponent c x of
+--       (y, c') -> case runNetwork n y of
+--         (z, n') -> (z, c' :& n')
 
 
