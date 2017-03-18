506d3d93fea69fd1d4904fcc4cfad7c94790c3ac
Justin Le <justin@jle.im>
2017-03-18 01:46:32 -0700

    pure operators

--- src/Learn/Neural.hs
+++ src/Learn/Neural.hs
@@ -62,24 +62,24 @@ class Component c where
 data HasState = NoState
               | SomeState
 
-data Layer :: Type -> (BShape Nat -> Type) -> BShape Nat -> BShape Nat -> Type where
-    LPure  :: (CState c b i o ~ 'Nothing) => CParam c b i o -> Layer c b i o
-    LState :: (CState c b i o ~ 'Just s ) => CParam c b i o -> s -> Layer c b i o
+data Layer :: Type -> HasState -> (BShape Nat -> Type) -> BShape Nat -> BShape Nat -> Type where
+    LPure  :: (CState c b i o ~ 'Nothing) => CParam c b i o -> Layer c r b i o
+    LState :: (CState c b i o ~ 'Just s ) => CParam c b i o -> s -> Layer c 'SomeState b i o
 
-instance Num (Layer c b i o)
+instance Num (Layer c r b i o)
 
 layerOp
-    :: forall s c b i o. Component c
-    => OpB s '[ b i, Layer c b i o ] '[ b o, Layer c b i o ]
+    :: forall s c r b i o. Component c
+    => OpB s '[ b i, Layer c r b i o ] '[ b o, Layer c r b i o ]
 layerOp = OpM $ \(I x :< I l :< Ø) -> case l of
     LPure p -> do
-      (I y :< Ø, gF) <- runOpM' (runComponent @c @s @b @i @o) (x ::< p ::< Ø)
+      (I y :< Ø, gF) <- runOpM' (runComponent @_ @_ @_ @_ @o) (x ::< p ::< Ø)
       let gF' = fmap (\case I dX :< I dP :< Ø -> I dX :< I (LPure dP) :< Ø)
               . gF
               . (\case dY :< _ :< Ø -> dY :< Ø)
       return (y ::< LPure p ::< Ø, gF')
     LState p s -> do
-      (I y :< I s' :< Ø, gF) <- runOpM' (runComponent @c @s @b @i @o) (x ::< p ::< s ::< Ø)
+      (I y :< I s' :< Ø, gF) <- runOpM' (runComponent @_ @_ @_ @_ @o) (x ::< p ::< s ::< Ø)
       let gF' = fmap (\case I dX :< I dP :< I dS :< Ø -> dX ::< LState dP dS ::< Ø)
               . gF
               . (\case dY :< Just (LState dP dS) :< Ø -> dY :< Just dS :< Ø
@@ -87,37 +87,70 @@ layerOp = OpM $ \(I x :< I l :< Ø) -> case l of
                 )
       return (y ::< LState p s' ::< Ø, gF')
 
+layerOpPure
+    :: forall s c b i o. Component c
+    => OpB s '[ b i, Layer c 'NoState b i o ] '[ b o ]
+layerOpPure = OpM $ \(I x :< I l :< Ø) -> case l of
+    LPure p -> do
+      (I y :< Ø, gF) <- runOpM' (runComponent @_ @_ @_ @_ @o) (x ::< p ::< Ø)
+      let gF' = fmap (\case I dX :< I dP :< Ø -> I dX :< I (LPure dP) :< Ø)
+              . gF
+      return (y ::< Ø, gF')
+
 data LChain :: Type where
     (:~) :: BShape Nat -> Type -> LChain
 
-data Network :: (BShape Nat -> Type) -> LChain -> [LChain] -> BShape Nat -> Type where
-    NetExt :: Layer c b i o
-           -> Network b (i ':~ c) '[] o
+data Network :: HasState -> (BShape Nat -> Type) -> LChain -> [LChain] -> BShape Nat -> Type where
+    NetExt :: Layer c r b i o
+           -> Network r b (i ':~ c) '[] o
     (:&)   :: Component d
-           => Layer c b i h
-           -> Network b (h ':~ d) hs               o
-           -> Network b (i ':~ c) ((h ':~ d) ': hs) o
+           => Layer c r b i h
+           -> Network r b (h ':~ d) hs               o
+           -> Network r b (i ':~ c) ((h ':~ d) ': hs) o
 
 networkOp
-    :: forall s b i c hs o. (Component c)
-    => OpB s '[ b i, Network b (i ':~ c) hs o ] '[ b o, Network b (i ':~ c) hs o ]
+    :: forall s r b i c hs o. Component c
+    => OpB s '[ b i, Network r b (i ':~ c) hs o ] '[ b o, Network r b (i ':~ c) hs o ]
 networkOp = OpM $ \(I x :< I n :< Ø) -> case n of
     NetExt l -> do
-      (I y :< I l' :< Ø, gF) <- runOpM' (layerOp @s @c @b @i @o) (x ::< l ::< Ø)
+      (I y :< I l' :< Ø, gF) <- runOpM' layerOp (x ::< l ::< Ø)
       let gF' = fmap (\case I dX :< I dL :< Ø -> dX ::< NetExt dL ::< Ø)
               . gF
               . (\case dY :< Just (NetExt dL) :< Ø -> dY :< Just dL :< Ø
                        dY :< Nothing          :< Ø -> dY :< Nothing :< Ø
                 )
       return (y ::< NetExt l' ::< Ø, gF')
-    (l :: Layer c b i h) :& (n2 :: Network b (h ':~ d) js o) -> do
-      (I y :< I l'  :< Ø, gF) <- runOpM' (layerOp @s @c @b @i) (x ::< l ::< Ø)
+    (l :: Layer c r b i h) :& (n2 :: Network r b (h ':~ d) js o) -> do
+      (I y :< I l'  :< Ø, gF ) <- runOpM' layerOp   (x ::< l ::< Ø)
       (I z :< I n2' :< Ø, gF') <- runOpM' networkOp (y ::< n2 ::< Ø)
-      let gF'' :: Prod Maybe '[ b o, Network b (i ':~ c) ((h ':~ d) ': js) o]
-               -> ST s (Tuple '[ b i, Network b (i ':~ c) ((h ':~ d) ': js) o])
+      let gF'' :: Prod Maybe '[ b o, Network r b (i ':~ c) ((h ':~ d) ': js) o]
+               -> ST s (Tuple '[ b i, Network r b (i ':~ c) ((h ':~ d) ': js) o])
           gF'' = \case dZ :< Just (dL :& dN) :< Ø -> do
                          I dY :< I dN2 :< Ø <- gF' (dZ :< Just dN :< Ø)
-                         I dX :< I dL0 :< Ø <- gF (Just dY :< Just dL :< Ø)
+                         I dX :< I dL0 :< Ø <- gF  (Just dY :< Just dL :< Ø)
+                         return $ dX ::< (dL0 :& dN2) ::< Ø
+                       dZ :< Nothing   :< Ø -> do
+                         I dY :< I dN2 :< Ø <- gF' (dZ :< Nothing :< Ø)
+                         I dX :< I dL0 :< Ø <- gF  (Just dY :< Nothing :< Ø)
                          return $ dX ::< (dL0 :& dN2) ::< Ø
       return (z ::< (l' :& n2') ::< Ø, gF'')
 
+networkOpPure
+    :: forall s b i c hs o. Component c
+    => OpB s '[ b i, Network 'NoState b (i ':~ c) hs o ] '[ b o ]
+networkOpPure = OpM $ \(I x :< I n :< Ø) -> case n of
+    NetExt l -> do
+      (I y :< Ø, gF) <- runOpM' layerOpPure (x ::< l ::< Ø)
+      let gF' = fmap (\case I dX :< I dL :< Ø -> dX ::< NetExt dL ::< Ø)
+              . gF
+      return (y ::< Ø, gF')
+    (l :: Layer c 'NoState b i h) :& (n2 :: Network 'NoState b (h ':~ d) js o) -> do
+      (I y :< Ø, gF ) <- runOpM' layerOpPure   (x ::< l ::< Ø)
+      (I z :< Ø, gF') <- runOpM' networkOpPure (y ::< n2 ::< Ø)
+      let gF'' :: Prod Maybe '[ b o ]
+               -> ST s (Tuple '[ b i, Network 'NoState b (i ':~ c) ((h ':~ d) ': js) o])
+          gF'' dZ = do
+            I dY :< I dN2 :< Ø <- gF' dZ
+            I dX :< I dL0 :< Ø <- gF  (Just dY :< Ø)
+            return $ dX ::< (dL0 :& dN2) ::< Ø
+      return (z ::< Ø, gF'')
