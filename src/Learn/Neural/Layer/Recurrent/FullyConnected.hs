63dc9bcf3f75553cabbead12cff7d01ada9e1231
Justin Le <justin@jle.im>
2017-03-22 19:50:43 -0700

    proper exports and num instances

--- src/Learn/Neural/Layer/Recurrent/FullyConnected.hs
+++ src/Learn/Neural/Layer/Recurrent/FullyConnected.hs
@@ -16,6 +16,9 @@
 
 module Learn.Neural.Layer.Recurrent.FullyConnected (
     FullyConnectedR
+  , FullyConnectedR'
+  , CommonMap(..)
+  , MapFunc(..)
   ) where
 
 import           Data.Kind
@@ -39,8 +42,25 @@ data FullyConnectedR :: Type
 deriving instance Generic (CParam FullyConnectedR b (BV i) (BV o))
 instance SOP.Generic (CParam FullyConnectedR b (BV i) (BV o))
 
-instance Num (CParam FullyConnectedR b (BV i) (BV o))
-instance Num (CState FullyConnectedR b (BV i) (BV o))
+instance (Num (b (BM o o)), Num (b (BM o i)), Num (b (BV o)))
+      => Num (CParam FullyConnectedR b (BV i) (BV o)) where
+    FCRP wI1 wS1 b1 + FCRP wI2 wS2 b2 = FCRP (wI1 + wI2) (wS1 + wS2) (b1 + b2)
+    FCRP wI1 wS1 b1 - FCRP wI2 wS2 b2 = FCRP (wI1 - wI2) (wS1 - wS2) (b1 - b2)
+    FCRP wI1 wS1 b1 * FCRP wI2 wS2 b2 = FCRP (wI1 * wI2) (wS1 * wS2) (b1 * b2)
+    negate (FCRP wI wS b) = FCRP (negate wI) (negate wS) (negate b)
+    signum (FCRP wI wS b) = FCRP (signum wI) (signum wS) (signum b)
+    abs    (FCRP wI wS b) = FCRP (abs    wI) (abs    wS) (abs    b)
+    fromInteger x = FCRP (fromInteger x) (fromInteger x) (fromInteger x)
+
+instance Num (b (BV o)) => Num (CState FullyConnectedR b (BV i) (BV o)) where
+    FCRS s1 + FCRS s2 = FCRS (s1 + s2)
+    FCRS s1 - FCRS s2 = FCRS (s1 - s2)
+    FCRS s1 * FCRS s2 = FCRS (s1 * s2)
+    negate (FCRS s) = FCRS (negate s)
+    signum (FCRS s) = FCRS (signum s)
+    abs    (FCRS s) = FCRS (abs    s)
+    fromInteger x  = FCRS (fromInteger x)
+
 
 instance (KnownNat i, KnownNat o) => Component FullyConnectedR (BV i) (BV o) where
     data CParam  FullyConnectedR b (BV i) (BV o) =
@@ -81,8 +101,25 @@ data FullyConnectedR' :: k -> Type
 deriving instance Generic (CParam (FullyConnectedR' c) b (BV i) (BV o))
 instance SOP.Generic (CParam (FullyConnectedR' c) b (BV i) (BV o))
 
-instance Num (CParam (FullyConnectedR' s) b (BV i) (BV o))
-instance Num (CState (FullyConnectedR' s) b (BV i) (BV o))
+instance (Num (b (BM o o)), Num (b (BM o i)), Num (b (BV o)))
+      => Num (CParam (FullyConnectedR' s) b (BV i) (BV o)) where
+    FCRP' wI1 wS1 b1 + FCRP' wI2 wS2 b2 = FCRP' (wI1 + wI2) (wS1 + wS2) (b1 + b2)
+    FCRP' wI1 wS1 b1 - FCRP' wI2 wS2 b2 = FCRP' (wI1 - wI2) (wS1 - wS2) (b1 - b2)
+    FCRP' wI1 wS1 b1 * FCRP' wI2 wS2 b2 = FCRP' (wI1 * wI2) (wS1 * wS2) (b1 * b2)
+    negate (FCRP' wI wS b) = FCRP' (negate wI) (negate wS) (negate b)
+    signum (FCRP' wI wS b) = FCRP' (signum wI) (signum wS) (signum b)
+    abs    (FCRP' wI wS b) = FCRP' (abs    wI) (abs    wS) (abs    b)
+    fromInteger x = FCRP' (fromInteger x) (fromInteger x) (fromInteger x)
+
+instance Num (b (BV o)) => Num (CState (FullyConnectedR' s) b (BV i) (BV o)) where
+    FCRS' s1 + FCRS' s2 = FCRS' (s1 + s2)
+    FCRS' s1 - FCRS' s2 = FCRS' (s1 - s2)
+    FCRS' s1 * FCRS' s2 = FCRS' (s1 * s2)
+    negate (FCRS' s) = FCRS' (negate s)
+    signum (FCRS' s) = FCRS' (signum s)
+    abs    (FCRS' s) = FCRS' (abs    s)
+    fromInteger x  = FCRS' (fromInteger x)
+
 
 instance (KnownNat i, KnownNat o, Reifies s MapFunc)
       => Component (FullyConnectedR' s) (BV i) (BV o) where
