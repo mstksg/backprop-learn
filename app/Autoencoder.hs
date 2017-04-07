67c2b08fa83479a820e7bd7de727805391bf03e8
Justin Le <justin@jle.im>
2017-04-06 21:26:32 -0700

    autoencoder for letters

--- app/Autoencoder.hs
+++ app/Autoencoder.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ApplicativeDo        #-}
 {-# LANGUAGE DataKinds            #-}
 {-# LANGUAGE GADTs                #-}
 {-# LANGUAGE LambdaCase           #-}
@@ -9,6 +10,7 @@
 
 
 -- import           Data.Type.Combinator
+import           Control.Applicative
 import           Control.DeepSeq
 import           Control.Exception
 import           Control.Monad.IO.Class
@@ -66,24 +68,26 @@ main = MWC.withSystemRandom $ \g -> do
     holmes <- evaluate . force . mapMaybe (charOneHot @HM)
         =<< readFile "data/holmes.txt"
     putStrLn "Loaded data"
-    let slices :: [(HM '[128], HM '[256])]
-        slices =
-          zipWith3 (\x y z -> (y, tload sing (textract x VSi.++ textract z)))
-            holmes (drop 1 holmes) (drop 2 holmes)
+    let slices :: [(HM '[128], HM '[128])]
+        slices = concat . getZipList $ do
+          skips <- traverse (ZipList . flip drop holmes) [0..2]
+          pure $ case skips of
+            [l1,l2,l3] ->
+              [(l2,l1),(l2,l3)]
+            _ -> []
     slices' <- liftIO . fmap V.toList $ MWC.uniformShuffle (V.fromList slices) g
     let (test,train) = splitAt (length slices `div` 50) slices'
-    -- mapM_ print slices
     net0 :: Network 'FeedForward HM
               ( '[128] :~ FullyConnected )
-             '[ '[16 ] :~ LogitMap
-              , '[16 ] :~ FullyConnected
-              , '[2  ] :~ LogitMap
-              , '[2  ] :~ FullyConnected
-              , '[24 ] :~ LogitMap
-              , '[24 ] :~ FullyConnected
-              , '[256] :~ LogitMap
+             '[ '[32 ] :~ LogitMap
+              , '[32 ] :~ FullyConnected
+              , '[3  ] :~ LogitMap
+              , '[3  ] :~ FullyConnected
+              , '[32 ] :~ LogitMap
+              , '[32 ] :~ FullyConnected
+              , '[128] :~ SoftMax '[128]
               ]
-             '[256] <- initDefNet g
+             '[128] <- initDefNet g
     flip evalStateT net0 . forM_ [1..] $ \e -> do
       train' <- liftIO . fmap V.toList $ MWC.uniformShuffle (V.fromList train) g
       liftIO $ printf "[Epoch %d]\n" (e :: Int)
@@ -94,16 +98,28 @@ main = MWC.withSystemRandom $ \g -> do
         printf "(Epoch %d, Batch %d / %d)\n" e (b :: Int) numChunks
 
         t0 <- getCurrentTime
-        n' <- evaluate $ optimizeList_ (bimap only_ only_ <$> chnk) n0
+        n' <- evaluate $ optimizeListBatches_ (bimap only_ only_ <$> chnk) n0
                                        -- (sgdOptimizer rate netOpPure crossEntropy)
-                                       (adamOptimizer def netOpPure crossEntropy)
+                                       (batching (adamOptimizer def netOpPure crossEntropy))
+                                       25
         t1 <- getCurrentTime
         printf "Trained on %d points in %s.\n" batch (show (t1 `diffUTCTime` t0))
 
-        let trainScore = testNetList crossEntropyTest (someNet n') chnk
-            testScore  = testNetList crossEntropyTest (someNet n') test
-        printf "Training Log Cross Entropy:   %.2f\n" (log trainScore)
-        printf "Validation Log Cross Entropy: %.2f\n" (log testScore)
+        let encoder :: Network 'FeedForward HM ( '[128] :~ FullyConnected )
+                         '[ '[32] :~ LogitMap, '[32] :~ FullyConnected, '[3] :~ LogitMap ]
+                         '[3]
+            encoder = takeNet known n'
+
+        forM_ [0..127] $ \(c :: ASCII) -> do
+          let enc :: HM '[3]
+              enc = runNetPure encoder (oneHot (c :< Ø))
+              [x,y,z] = toList $ textract enc
+          printf "%s\t%.4f\t%.4f\t%.4f\n" (show (toChar c)) x y z
+
+        let trainScore = testNetList maxTest (someNet n') chnk
+            testScore  = testNetList maxTest (someNet n') test
+        printf "Training Score:   %.2f%%\n" ((1 - trainScore) * 100)
+        printf "Validation Score: %.2f%%\n" ((1 - testScore ) * 100)
 
         return ((), n')
   where
