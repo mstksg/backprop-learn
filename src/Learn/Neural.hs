44b29e22e5c11abd0f488159b050bc4a9f271211
Justin Le <justin@jle.im>
2017-04-20 00:49:42 -0700

    dropout module in main export

--- src/Learn/Neural.hs
+++ src/Learn/Neural.hs
@@ -10,6 +10,7 @@ import           Learn.Neural.Layer.Identity       as N
 import           Learn.Neural.Layer.Mapping        as N
 import           Learn.Neural.Loss                 as N
 import           Learn.Neural.Network              as N
+import           Learn.Neural.Network.Dropout      as N
 import           Learn.Neural.Test                 as N
 import           Learn.Neural.Train                as N
-import           Numeric.BLAS                as N
+import           Numeric.BLAS                      as N
