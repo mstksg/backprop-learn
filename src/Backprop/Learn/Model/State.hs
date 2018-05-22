fc89f18c437456e06caa9a0ed2c472f25e1971d6
Justin Le <justin@jle.im>
2018-05-21 22:15:32 -0700

    more translation to withModelFunc style

--- src/Backprop/Learn/Model/State.hs
+++ src/Backprop/Learn/Model/State.hs
@@ -48,14 +48,9 @@ trainState
      )
     => Model           p s           a b
     -> Model (TupMaybe p s) 'Nothing a b
-trainState f = Model
-    { runLearn = \ps x n_ ->
-        let (p, s) = splitTupMaybe @_ @p @s (\(T2B v u) -> (v, u)) ps
-        in  (second . const) n_ $ runLearn f p x s
-    , runLearnStoch = \g ps x n_ ->
-        let (p, s) = splitTupMaybe @_ @p @s (\(T2B v u) -> (v, u)) ps
-        in  (second . const) n_ <$> runLearnStoch f g p x s
-    }
+trainState = withModelFunc $ \f ps x n_ ->
+    let (p, s) = splitTupMaybe @_ @p @s (\(T2B v u) -> (v, u)) ps
+    in  (second . const) n_ <$> f p x s
 
 -- | Make a model stateless by pre-applying a fixed state (or a stochastic
 -- one with fixed stribution) and dropping the modified state from the
@@ -107,18 +102,12 @@ unroll
     :: (Traversable t, Backprop a, Backprop b, Backprop (t b))
     => Model p s    a     b
     -> Model p s (t a) (t b)
-unroll f = Model
-    { runLearn      = \  p xs s -> first collectVar
-                                 . flip runState s
-                                 . traverse (state . runLearn f p)
-                                 . sequenceVar
-                                 $ xs
-    , runLearnStoch = \g p xs s -> (fmap . first) collectVar
-                                 . flip runStateT s
-                                 . traverse (StateT . runLearnStoch f g p)
-                                 . sequenceVar
-                                 $ xs
-    }
+unroll = withModelFunc $ \f p xs s ->
+            (fmap . first) collectVar
+          . flip runStateT s
+          . traverse (StateT . f p)
+          . sequenceVar
+          $ xs
 
 -- | Version of 'unroll' that only keeps the "final" result, dropping all
 -- of the intermediate results.
@@ -132,16 +121,10 @@ unrollFinal
     :: (Traversable t, Backprop a)
     => Model p s    a  b
     -> Model p s (t a) b
-unrollFinal f = Model
-    { runLearn      = \  p xs s0 ->
-          foldl' (\(_, s) x -> runLearn f p x s)
-                 (undefined, s0)        -- TODO: error
-                 (sequenceVar xs)
-    , runLearnStoch = \g p xs s0 ->
-          foldlM (\(_, s) x -> runLearnStoch f g p x s)
-                 (undefined, s0)
-                 (sequenceVar xs)
-    }
+unrollFinal = withModelFunc $ \f p xs s0 ->
+    foldlM (\(_, s) x -> f p x s)
+           (undefined, s0)
+           (sequenceVar xs)
 
 -- | Fix a part of a parameter of a model to be (a function of) the
 -- /previous/ ouput of the model itself.
@@ -167,27 +150,22 @@ recurrent
     -> BFunc c b                                -- ^ store state
     -> Model p           s            ab c
     -> Model p (TupMaybe s ('Just b)) a  c
-recurrent spl joi sto f = Model
-    { runLearn = \p x sy ->
-        let (s, J_ y) = splitTupMaybe @_ @s @('Just b) (\(T2B v u) -> (v, u)) sy
-            xy        = isoVar2 joi spl x y
-            (z, s')   = runLearn f p xy s
-        in  (z, tupMaybe T2B s' (J_ (sto z)))
-    , runLearnStoch = \g p x sy -> do
-        let (s, J_ y) = splitTupMaybe @_ @s @('Just b) (\(T2B v u) -> (v, u)) sy
-            xy        = isoVar2 joi spl x y
-        (z, s') <- runLearnStoch f g p xy s
-        pure (z, tupMaybe T2B s' (J_ (sto z)))
-    }
+recurrent spl joi sto = withModelFunc $ \f p x sy -> do
+    let (s, J_ y) = splitTupMaybe @_ @s @('Just b) (\(T2B v u) -> (v, u)) sy
+        xy        = isoVar2 joi spl x y
+    (z, s') <- f p xy s
+    pure (z, tupMaybe T2B s' (J_ (sto z)))
 
 -- | Give a stateless model a "dummy" state.  For now, useful for using
 -- with combinators like 'deState' that require state.  However, 'deState'
 -- could also be made more lenient (to accept non stateful models) in the
 -- future.
+--
+-- Also useful for usage with combinators like 'Control.Category..' from
+-- "Control.Category" that requires all input models to share common state.
 dummyState
-    :: Model p 'Nothing   a b
+    :: forall s p a b. ()
+    => Model p 'Nothing   a b
     -> Model p ('Just s) a b
-dummyState f = Model
-    { runLearn      = \  p x s -> (second . const) s  $ runLearn f p x N_
-    , runLearnStoch = \g p x s -> (second . const) s <$> runLearnStoch f g p x N_
-    }
+dummyState = withModelFunc $ \f p x s ->
+    (second . const) s <$> f p x N_
